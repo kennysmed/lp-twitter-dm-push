0e25d180e0f80f51e9e6543cb4eefefcfc2c0bd8
Phil Gyford <phil@gyford.com>
2013-05-09 18:18:08 +0100

    Maybe fix Twitter keys?

--- ahola/twitter.rb
+++ ahola/twitter.rb
@@ -4,34 +4,31 @@ require 'tweetstream'
 module Ahola 
   class Twitter
 
-    def initialize
-      puts "KEY: " + ENV['TWITTER_CONSUMER_KEY']
-      @consumer_key = ENV['TWITTER_CONSUMER_KEY']
-      @consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
-    end
+      def initialize
+        @config = YAML.load_file('auth.yml') if File.exists?('auth.yml')
+      end
 
-    # def [](key)
-    #   key = "twitter_#{key}"
-    #   ENV[key.upcase] || @config[key]
-    # end
+      def [](key)
+        key = "twitter_#{key}"
+        ENV[key.upcase] || @config[key]
+      end
 
-  
-    def self.consumer
-      puts "KEY 2: " + @consumer_key
-      return OAuth::Consumer.new(
-        @consumer_key,
-        @consumer_secret,
-        :site => "https://api.twitter.com")
-    end
 
-    def self.tweetstream(token, secret)
-      TweetStream::Client.new(
-        :consumer_key => consumer.key,
-        :consumer_secret => consumer.secret,
-        :oauth_token => token,
-        :oauth_token_secret => secret,
-        :auth_method => :oauth
-      )
-    end
+      def self.consumer
+        config = new
+        return OAuth::Consumer.new(config[:consumer_key],
+          config[:consumer_secret],
+          :site => "https://api.twitter.com")
+      end
+
+      def self.tweetstream(token, secret)
+        TweetStream::Client.new(
+          :consumer_key => consumer.key,
+          :consumer_secret => consumer.secret,
+          :oauth_token => token,
+          :oauth_token_secret => secret,
+          :auth_method => :oauth
+        )
+      end
   end
 end
