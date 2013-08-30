b3915b4609da392088f5bd48bcc015046b6f4340
Phil Gyford <phil@gyford.com>
2013-08-30 10:26:32 +0100

    Fix error in config

--- ahola/config.rb
+++ ahola/config.rb
@@ -3,13 +3,13 @@ require 'yaml'
 module Ahola
   class Config
 
-    def self.config
+    def config
       @config ||= YAML.load_file('config.yml') if File.exists?('config.yml')
     end
 
     def [](key)
       key = "#{key}"
-      ENV[key.upcase] || config[key]
+      ENV[key.upcase] || self.config[key]
     end
   end
 end
