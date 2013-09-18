84a9d2419e51f347f957eed798622149e5723ee1
Phil Gyford <phil@gyford.com>
2013-09-18 16:47:14 +0100

    Add empty background tests

---
+++ spec/background_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+require 'ahola/background'
+
+
+describe "Background" do
+  
+  # Going to leave this for the moment, as we'll be changing all the Streaming
+  # stuff to use Site Streams.
+
+end
+
